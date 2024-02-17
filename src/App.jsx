bfbf06e04995b5085c52854227e6ce297a3f13af
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-02-17 14:43:27 +0200

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import SideNavbar from './components/SideNavbar'
+import Main from "./components/Main";
+import SideNavbar from "./components/SideNavbar";
 
 function App() {
-
   return (
-    <>
-<SideNavbar/>
-    </>
-  )
+    <div>
+      <SideNavbar />
+      <Main/>
+    </div>
+  );
 }
 
-export default App
+export default App;
