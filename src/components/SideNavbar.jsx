53e69c107f558bc17dd436d84877f676051034a4
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-02-18 08:56:13 +0200

    onClick to close navigation menu

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -10,47 +10,48 @@ import { GrProjects } from "react-icons/gr";
 
 const SideNavbar = () => {
   const [navbar, setNavbar] = useState(false);
-  const hadleNavbar = () => {
+
+  const handleNavbar = () => {
     setNavbar(!navbar);
   };
 
   return (
     <div>
       <AiOutlineMenu
-        onClick={hadleNavbar}
+        onClick={handleNavbar}
         className="absolute top-4 right-4 z-[99] md:hidden"
       />
       {navbar ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-          <a
+          <a onClick={handleNavbar}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineHome size={20} />
             <span className="pl-4 ">Home</span>
           </a>
-          <a
+          <a onClick={handleNavbar}
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
             <span className="pl-4 ">Work</span>
           </a>
-          <a
+          <a onClick={handleNavbar}
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineProject size={20} />
             <span className="pl-4 ">Projects</span>
           </a>
-          <a
+          <a onClick={handleNavbar}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20} />
             <span className="pl-4 ">Resume</span>
           </a>
-          <a
+          <a onClick={handleNavbar}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
