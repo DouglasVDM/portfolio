af8d2cb360a1e442e43eba9b44ead0b18ba18f44
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-02-18 05:58:26 +0200

    Work and project components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Main from "./components/Main";
+import Projects from "./components/Projects";
 import SideNavbar from "./components/SideNavbar";
+import Work from "./components/Work";
 
 function App() {
   return (
     <div>
       <SideNavbar />
       <Main/>
+      <Work/>
+      <Projects/>
     </div>
   );
 }
