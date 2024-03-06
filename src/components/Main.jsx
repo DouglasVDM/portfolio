91f3051f37c12940fe49d0804d8a6327bc5caa92
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-03-06 23:18:17 +0200

    Onclick to open links in a new tab

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,46 +1,42 @@
 import { TypeAnimation } from "react-type-animation";
-import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import douglasImg from "../assets/douglas.jpg";
 
 const Main = () => {
+  const handleLinkClick = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <div id="main">
       <img
         className="w-full h-screen object-cover object-left scale-x-[-1]"
-        src="/home/douglas/Pictures/Screenshots"
+        src={douglasImg}
       />
-      {/* https://www.istockphoto.com/photo/trail-running-gm875551414-244422715?phrase=trail%20running" /> */}
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">Im Douglas</h1>
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center text-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+            Douglas Van Der Merwe
+          </h1>
           <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
-            Im a
-            <TypeAnimation
-              sequence={[
-                // Same substring at the start will only be typed out once, initially
-                "Developer",
-                2000, // wait 1s before replacing "Mice" with "Hamsters"
-                "Coder",
-                2000,
-                "Tech Enthusiast",
-                2000,
-                "Cloud Enthusiast",
-                2000,
-              ]}
-              wrapper="div"
-              speed={50}
-              style={{
-                fontSize: "1em",
-                display: "inline-block",
-                paddingLeft: "5px",
-              }}
-              repeat={Infinity}
-            />
+            I am the one who takes on that task that everyone avoids, and gets
+            it done.
           </h2>
           <div className="flex justify-between pt-6 max-w-[200px] w-full">
-            <FaTwitter className="cursor-pointer" size={20} />
-            <FaFacebook className="cursor-pointer" size={20} />
-            <FaInstagram className="cursor-pointer" size={20} />
-            <FaLinkedin className="cursor-pointer" size={20} linkto="https://www.linkedin.com/in/douglas-van-der-merwe-b4552467/" />
+            <FaGithub
+              className="cursor-pointer"
+              size={20}
+              onClick={() =>
+                handleLinkClick("https://www.github.com/DouglasVDM/")
+              }
+            />
+            <FaLinkedin
+              className="cursor-pointer"
+              size={20}
+              onClick={() =>
+                handleLinkClick("https://www.linkedin.com/in/douglas-van-der-merwe-b4552467/")
+              }
+            />
           </div>
         </div>
       </div>
