68831dd13a1f135444fa55b086083df5633441fb
Douglas Van Der Merwe <douglasvdmerwe@gmail.com>
2024-03-12 08:48:03 +0000

    Add website link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,20 +11,61 @@ const Projects = () => {
       </h1>
       <p className="text-center py-8">
         My main focus is to add value. Achieved by solving problems that may or
-        may not have been identified. This involves contributing to open source projects,
-        adding chapters to YouTube videos, joining traininees on projects where
-        help is needed, etc.
+        may not have been identified. This involves contributing to open source
+        projects, adding chapters to YouTube videos, joining traininees on
+        projects where help is needed, etc.
       </p>
       <div className="grid sm:grid-cols-2 gap-12">
         <ProjectItem
           img={redstorImg}
-          title="Redstor"
+          title={
+            <a
+              href="https://www.youtube.com/watch?v=JTqEojjT5pI&t=69s"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                color: "blue",
+                textDecoration: "underline",
+                cursor: "pointer",
+              }}
+            >
+              Redstor
+            </a>
+          }
           projectInfo="Added timestamps to most recent YouTube video"
         />
         <ProjectItem
           img={freecodecampImg}
-          title="FreeCodeCamp"
-          projectInfo="Onboarded as contributor on their open source project and Added timestamps to YouTube videos"
+          title={
+            <a
+              href="https://contribute.freecodecamp.org/#/"
+              target="_blank"
+              rel="noopener nereferrer"
+              style={{
+                color: "blue",
+                textDecoration: "underline",
+                cursor: "pointer",
+              }}
+            >
+              FreeCodeCamp
+            </a>
+          }
+          projectInfo=<span>
+            Onboarded as contributor on their open source project and added {" "}
+            <a
+              href="https://www.youtube.com/watch?v=imqiYWidUIA&t=386s"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                color: "blue",
+                textDecoration: "underline",
+                cursor: "pointer",
+              }}
+            >
+              timestamps
+            </a>
+            {" "}to their YouTube videos
+          </span>
         />
         <ProjectItem
           img={mayfieldImg}
